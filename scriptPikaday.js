6c7b3cb0bf0ba3672d7e81ed7ff63882c0599c3b
pedrohcalado <pedrocalado22@gmail.com>
2020-06-05 15:03:09 -0300

    Ajusta code climate

--- scriptPikaday.js
+++ scriptPikaday.js
@@ -1,6 +1,6 @@
 const picker = new Pikaday({
   field: document.getElementById('datepicker'),
-  toString(date, format) {
-    return moment(date).format(' DD / MM / YYYY');
-  }
+  toString(date) {
+    return moment(date).format('DD/MM/YYYY');
+  },
 });
